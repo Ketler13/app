4b724edd3082fd468cb81bd905194e621a4d2988
Ketler13 <ketler13@yandex.ru>
2017-02-26 01:25:51 +0200

    add button to detailform

--- src/components/DetailForm.js
+++ src/components/DetailForm.js
@@ -1,5 +1,7 @@
 import React, {Component, PropTypes} from 'react'
-import Slider from 'material-ui/Slider';
+import Slider from 'material-ui/Slider'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 export default class DetailForm extends Component {
     state = {
@@ -13,37 +15,6 @@ export default class DetailForm extends Component {
 
     handleTimesSlider = (event, value) => {
         this.setState({times: value})
-    }
-
-    render() {
-        return (
-            <div>
-                <Slider
-                    min={0}
-                    max={200}
-                    step={1}
-                    defaultValue={50}
-                    value={this.state.weight}
-                    onChange={this.handleWeightSlider}
-                />
-                <Slider
-                    min={0}
-                    max={30}
-                    step={1}
-                    defaultValue={15}
-                    value={this.state.times}
-                    onChange={this.handleTimesSlider}
-                />
-                <p>weight {this.state.weight} times {this.state.times}</p>
-                <button onClick = {this.handleClick}>OK</button>
-            </div>
-        )
-    }
-
-    handleChange = field => ev => {
-        this.setState({
-            [field]: ev.target.value
-        })
     }
 
     handleClick = ev => {
@@ -60,5 +31,44 @@ export default class DetailForm extends Component {
             weight: 0,
             times: 0
         })
+    }
+
+    render() {
+        const { weight, times } = this.state
+        const disabled = !(weight && times)
+        const style = {
+            margin: '5px 0 0 50%',
+            transform: 'translateX(-50%)'
+        }
+        return (
+            <div>
+                <Slider
+                    min={0}
+                    max={200}
+                    step={1}
+                    defaultValue={50}
+                    value={this.state.weight}
+                    onChange={this.handleWeightSlider}
+                />
+                <p className = 'detailFormLabel'>{this.state.weight} kg</p>
+                <Slider
+                    min={0}
+                    max={30}
+                    step={1}
+                    defaultValue={15}
+                    value={this.state.times}
+                    onChange={this.handleTimesSlider}
+                />
+                <p className = 'detailFormLabel'>{this.state.times} times</p>
+                <FloatingActionButton
+                    secondary = {disabled ? false : true}
+                    disabled = {disabled ? true : false}
+                    onTouchTap = {this.handleClick}
+                    style = {style}
+                >
+                    <ContentAdd />
+                </FloatingActionButton>
+            </div>
+        )
     }
 }
