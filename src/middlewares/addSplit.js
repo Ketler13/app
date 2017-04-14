9d70a0bb50c27e7597e5b328962dc863fef52557
Ketler13 <ketler13@yandex.ru>
2017-04-14 22:30:27 +0300

    first step to real api

---
+++ src/middlewares/addSplit.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export default store => next => action => {
+    const {generateId, ...rest} = action
+    if (generateId) {
+      axios({
+        method: 'post',
+        url: '/api/addSplit',
+        data: JSON.stringify(rest.payload),
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+      .then(response => console.log(JSON.parse(response.request.response)))
+      axios({
+        method: 'get',
+        url: '/api/splits',
+        data: JSON.stringify(rest.payload)
+      })
+      .then(response => console.log(JSON.parse(response.request.response)))
+    }
+    next(action)
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from '../reducer'
 import randomId from '../middlewares/randomId'
+import addSplit from '../middlewares/addSplit'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancer = composeEnhancers(applyMiddleware(randomId))
+const enhancer = composeEnhancers(applyMiddleware(addSplit, randomId))
 
 const store = createStore(reducer, {}, enhancer)
 window.store = store
